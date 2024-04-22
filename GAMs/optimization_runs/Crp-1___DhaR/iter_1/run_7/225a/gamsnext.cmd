6563d34842b7fe2584b168081ff8c35ef675583e
cdalldorf <36520023+cdalldorf@users.noreply.github.com>
2024-04-22 15:19:17 -0700

    cleaning

--- GAMs/optimization_runs/Crp-1___DhaR/iter_1/run_7/225a/gamsnext.cmd
+++
@@ -1,2 +0,0 @@
-@echo off
-gmsgennt.cmd "C:\Users\SBRG_2024\Documents\GitHub\regulonML\GAMs\optimization_runs\Crp-1___DhaR\iter_1\run_7\225a\" "C:\Users\SBRG_2024\Documents\GitHub\regulonML\GAMs\optimization_runs\Crp-1___DhaR\iter_1\run_7\" %1 "C:\Users\SBRG_2024\Documents\GitHub\regulonML\GAMs\optimization_runs\Crp-1___DhaR\iter_1\run_7\225a\gamscntr.dat" "C:\GAMS\win64\28.2\" "CONOPT"
